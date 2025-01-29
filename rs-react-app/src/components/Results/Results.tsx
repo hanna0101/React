2cdc705f08e6c8c31c34d952370e000d96cadaa9
hanna0101 <110972237+hanna0101@users.noreply.github.com>
2025-01-29 11:44:01 +0200

    refactor: refactor results component

--- rs-react-app/src/components/Results/Results.tsx
+++ rs-react-app/src/components/Results/Results.tsx
@@ -4,35 +4,39 @@ import { CardItem } from '../CardItem/CardItem';
 import Spinner from '../Spinner/Spinner.tsx';
 
 export default class Results extends Component<any, any> {
+  extractIdFromUrl = (url) => {
+    const match = url.match(/\/(\d+)\/$/);
+    return match ? parseInt(match[1], 10) : null;
+  };
+
+  renderResults = () => {
+    const { searchResults } = this.props;
+
+    return searchResults.map((el) => {
+      const { birth_year, name, gender, eye_color, url } = el;
+      const id = this.extractIdFromUrl(url);
+
+      return id ? (
+        <CardItem
+          key={id}
+          img={`https://starwars-visualguide.com/assets/img/characters/${id}.jpg`}
+          birthYear={birth_year}
+          name={name}
+          gender={gender}
+          eyeColor={eye_color}
+        />
+      ) : null;
+    });
+  };
   render() {
-    const { searchResults, isLoading } = this.props;
+    const { isLoading } = this.props;
 
     return (
       <div className="containerResults">
         <h1>Results</h1>
-
-        {isLoading ? (
-          <Spinner />
-        ) : (
-          <div className="containerItems">
-            {searchResults.map((el) => {
-              const { birth_year, name, gender, eye_color, url } = el;
-              const match = url.match(/\/(\d+)\/$/);
-              const id = match ? parseInt(match[1], 10) : null;
-
-              return id ? (
-                <CardItem
-                  key={id}
-                  img={`https://starwars-visualguide.com/assets/img/characters/${id}.jpg`}
-                  birthYear={birth_year}
-                  name={name}
-                  gender={gender}
-                  eyeColor={eye_color}
-                />
-              ) : null;
-            })}
-          </div>
-        )}
+        <div className="containerItems">
+          {isLoading ? <Spinner /> : this.renderResults()}
+        </div>
       </div>
     );
   }
