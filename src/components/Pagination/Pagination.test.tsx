39e5789a36fd7577a4c39f2aed33a8245572a6fa
hanna0101 <110972237+hanna0101@users.noreply.github.com>
2025-02-09 20:18:07 +0200

    feat: add tests for pagination

--- src/components/Pagination/Pagination.test.tsx
+++ src/components/Pagination/Pagination.test.tsx
@@ -1,13 +1,19 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom';
-import { Pagination } from './Pagination.tsx';
+import { Pagination } from './Pagination';
 import { jest } from '@jest/globals';
 
+const mockNavigate = jest.fn();
+
 jest.mock('react-router', () => ({
-  useNavigate: jest.fn(),
+  useNavigate: () => mockNavigate,
 }));
 
 describe('Pagination', () => {
+  beforeEach(() => {
+    mockNavigate.mockClear();
+  });
+
   it('renders without crashing', () => {
     const mockSetSearchTerm = jest.fn();
 
@@ -22,5 +28,43 @@ describe('Pagination', () => {
 
     expect(screen.getByText('Previous')).toBeInTheDocument();
     expect(screen.getByText('Next')).toBeInTheDocument();
+  });
+
+  it('url updates after clicking Next button', () => {
+    const mockSetCurrentPage = jest.fn();
+
+    render(
+      <Pagination
+        nextPage={'2'}
+        previousPage={null}
+        currentPage={1}
+        setCurrentPage={mockSetCurrentPage}
+      />
+    );
+
+    const nextButton = screen.getByText('Next');
+    fireEvent.click(nextButton);
+
+    expect(mockNavigate).toHaveBeenCalledWith('/page/2');
+    expect(mockSetCurrentPage).toHaveBeenCalled();
+  });
+
+  it('url updates after clicking Previous button', () => {
+    const mockSetCurrentPage = jest.fn();
+
+    render(
+      <Pagination
+        nextPage={'2'}
+        previousPage={'1'}
+        currentPage={2}
+        setCurrentPage={mockSetCurrentPage}
+      />
+    );
+
+    const previousButton = screen.getByText('Previous');
+    fireEvent.click(previousButton);
+
+    expect(mockNavigate).toHaveBeenCalledWith('/page/1');
+    expect(mockSetCurrentPage).toHaveBeenCalled();
   });
 });
